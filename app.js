a9e4ab064c4f5cd6ae4da074717ee8202326e6e3
Jalal B Manafi <jalalbmnf@Jalals-MacBook-Pro.local>
2020-11-04 21:22:16 +0400

    querystring&POSTreqs

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ var express = require("express");
 var app = express();
 
 app.set("view engine", "ejs");
+app.use('/assets',express.static("assets"));
 
 app.get("/", function (req, res) {
-  res.render("/index");
+  res.render("index");
 });
 
 app.get("/contact", function (req, res) {
-  res.render("contact");
+  res.render("contact",{qs:req.query});
 });
 
-app.get("/profile/:name", function (req, res) {
+app.get("/profile/:id", function (req, res) {
   var data = {age:23,job:'developer',hobbies:["swimming","fighting","walking"]};
-  res.render("profile", { person: req.params.name ,data:data});
+  res.render("profile", { person: req.params.id ,data:data});
 });
 
 app.listen(1903);
