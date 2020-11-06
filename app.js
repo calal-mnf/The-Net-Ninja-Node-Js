3684220028e1cc1f0ae1d4cb401e22af33b9c881
calalmnf <calalmnf@gmail.com>
2020-11-06 09:09:16 +0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("/index");
+  res.render("index");
 });
 
 app.get("/contact", function (req, res) {
@@ -13,8 +13,12 @@ app.get("/contact", function (req, res) {
 });
 
 app.get("/profile/:name", function (req, res) {
-  var data = {age:23,job:'developer',hobbies:["swimming","fighting","walking"]};
-  res.render("profile", { person: req.params.name ,data:data});
+  var data = {
+    age: 23,
+    job: "developer",
+    hobbies: ["swimming", "fighting", "walking"],
+  };
+  res.render("profile", { person: req.params.name, data: data });
 });
 
 app.listen(1903);
